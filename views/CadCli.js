040f698b79ff5991118714405bd292c62c1c4e97
Julio Varella <julio.varella@unesp.br>
2021-01-07 22:05:55 -0300

    scroll cadcli

--- views/CadCli.js
+++ views/CadCli.js
@@ -1,48 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View, TouchableOpacity, TextInput,Alert, Image } from 'react-native';
+import { Text, View, TouchableOpacity, TextInput,Alert, Image, SafeAreaView, ScrollView} from 'react-native';
 import {css} from '../assets/css/Css';
 
 export default function CadCli(props){
     return(
-        <View style={css.container}>
+        <SafeAreaView style={css.container2}>
+        <ScrollView style={css.scrollView}>
+            <View style={css.container}> 
+                <Image
+                    source={require('../assets/img/icon.png')}   
+                    style={css.logo} 
+                />
 
-            <Image
-                source={require('../assets/img/icon.png')}   
-                style={css.logo} 
-            />
+                <TextInput
+                    style={css.input}
+                    placeholder='Nome de Usuário'
+                />
 
-            <TextInput
-                style={css.input}
-                placeholder='Nome de Usuário'
-            />
+                <TextInput
+                    style={css.input}
+                    placeholder='Senha'
+                    secureTextEntry={true}
+                />
 
-            <TextInput
-                style={css.input}
-                placeholder='Senha'
-                secureTextEntry={true}
-            />
+                <TextInput
+                    style={css.input}
+                    placeholder='Nome Completo'
+                />
 
-            <TextInput
-                style={css.input}
-                placeholder='Nome Completo'
-            />
+                <TextInput
+                    style={css.input}
+                    keyboardType='numeric'
+                    maxLength={9}
+                    placeholder='Número de Celular'
+                />
+            
+                 <TextInput
+                    style={css.input}
+                    keyboardType='numeric'
+                    maxLength={9}
+                    placeholder='RA'
+                />
 
-            <TextInput
-                style={css.input}
-                keyboardType='numeric'
-                maxLength={9}
-                placeholder='Número de Celular'
-            />
-        
+                <TextInput
+                    style={css.input}
+                    keyboardType='numeric'
+                    maxLength={9}
+                    placeholder='RA'
+                />
 
-            <TouchableOpacity
-                style={css.botao}
-                onPress={()=>{Alert.alert('Criando conta')}}
-            >
-            <Text style={css.textoBotao}>Criar</Text>
-            </TouchableOpacity>
+                <TextInput
+                    style={css.input}
+                    keyboardType='numeric'
+                    maxLength={9}
+                    placeholder='RA'
+                />
 
-        </View>
+                <TextInput
+                    style={css.input}
+                    keyboardType='numeric'
+                    maxLength={9}
+                    placeholder='RA'
+                />
+                
+                <TouchableOpacity
+                    style={css.botao}
+                    onPress={()=>{Alert.alert('Criando conta')}}
+                >
+                <Text style={css.textoBotao}>Criar</Text>
+                </TouchableOpacity>
+
+            </View> 
+        </ScrollView>
+        </SafeAreaView>
     );
 }
